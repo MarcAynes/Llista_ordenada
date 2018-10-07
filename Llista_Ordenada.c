5ffd1c160391ea8d2daa23f224ac74568f2e67d0
MarcAynes <marcaynes2@gmail.com>
2018-10-07 16:55:27 +0200

    feat  coded function PDI_crea
    created the function that allows you to create a list with the ghost
    element

---
+++ Llista_Ordenada.c
@@ -0,0 +1,17 @@
+#include "Llista_PDI.h"
+
+Llista_PDI PDI_crea(){
+    Llista_PDI llista;
+    llista.ant = llista.pri = NULL;
+    llista.pri = (Node*)malloc(sizeof(Node));
+    llista.ant = llista.pri;
+    if(llista.pri == NULL){
+        printf("\n\aError en la inisialitzacio del programa\n");
+    }
+    else{
+        llista.ant->seg = NULL;
+        llista.ant->X = -1;
+        llista.ant->Y = -1;
+    }
+    return llista;
+}
