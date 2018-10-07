d4d1cb1750ba3e91aa603948f9178aad450f2831
MarcAynes <marcaynes2@gmail.com>
2018-10-07 16:43:19 +0200

    feat  added headers on the .h file
    added the header of the functions in Llista_Ordenada.h with 2 structs
    Node and Llista_PDI where we have the information in the first and the
    list in the seconone

---
+++ Llista_Ordenada.h
@@ -0,0 +1,31 @@
+#ifndef _LLISTA_PDI_
+#define _LLISTA_PDI_
+
+#include<stdio.h>
+
+typedef struct N{
+    int Numero;
+    struct N *seg;
+    struct N *ant;
+}Node;
+
+typedef struct{
+    Node *pri;
+    Node *ant;
+}Llista_PDI;
+
+Llista_PDI PDI_crea();
+
+void PDI_escriu(Llista_PDI *llista, int X, int Y);
+
+void PDI_avanca(Llista_PDI *llista);
+
+void PDI_retrocedeix(Llista_PDI *llista);
+
+Node PDI_llegeix(Llista_PDI llista);
+
+void PDI_destrueix(Llista_PDI *llista);
+
+void PDI_vesInici(Llista_PDI *llista);
+
+#endif
